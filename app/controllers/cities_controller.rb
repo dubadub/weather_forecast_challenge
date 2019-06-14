0b5b83a48b4326c9af9675feef62885f068170df
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2019-06-14 18:47:48 +0100

    make it work: cities autocomplete

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,11 @@
 class CitiesController < ApplicationController
   def index
+    @cities = City.search(query_param).limit(10)
+  end
+
+  private
+
+  def query_param
+    params.permit(:q).fetch(:q)
   end
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -2,4 +2,6 @@ class City < ApplicationRecord
 
   validates :name, :openweathermap_city_id, presence: true
 
+  scope :search, ->(q) { q.present? ? where("lower(name) ILIKE ?", "%#{q.downcase}%") : none }
+
 end
